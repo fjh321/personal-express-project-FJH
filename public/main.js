7be026072e2e31c07bf44f294dca736e34cfdd82
Fernando Jesus Hernandez <fernandoj.hernandez123@gmail.com>
2023-11-12 22:48:14 -0500

    Personal Express Project

--- public/main.js
+++ public/main.js
@@ -1,45 +1,62 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
+const likes = document.getElementsByClassName("fa-thumbs-up");
 
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
+const trash = document.getElementsByClassName("fa-trash");
+
+Array.from(likes).forEach(function (element) {
+  element.addEventListener('click', function () {
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const rating = this.parentNode.parentNode.childNodes[3].innerText
+    const bookReport = this.parentNode.parentNode.childNodes[5].innerText
+    const likes = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
+    console.log(name)
+    console.log(rating)
+    console.log(bookReport)
+    console.log(likes)
+    fetch('messages', {
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        'name': name,
+        'rating': rating,
+        'bookReport': bookReport,
+        'likes': likes
+      })
+    })
+      .then(response => {
+        if (response.ok) return response.json()
+      })
+      .then(data => {
+        // console.log(data)
+        window.location.reload()
+      })
+  });
 });
 
-Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
+
+
+Array.from(trash).forEach(function (element) {
+  element.addEventListener('click', function () {
+    const name = this.closest('li').childNodes[1].innerText
+    const rating = this.closest('li').childNodes[3].innerText
+    const bookReport = this.closest('li').childNodes[5].innerText
+    const likes = parseFloat(this.closest('li').childNodes[7].innerText)
+    console.log(name)
+    console.log(rating)
+    console.log(bookReport)
+    console.log(likes)
+    fetch('messages', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'name': name,
+        'rating': rating,
+        'bookReport': bookReport,
+        'likes': likes
+      })
+    }).then(function (response) {
+      window.location.reload()
+    })
+  });
 });
--- server.js
+++ server.js
@@ -3,35 +3,46 @@ const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
-var db, collection;
+require('dotenv').config()
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
 
-app.listen(3000, () => {
-    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        db = client.db(dbName);
-        console.log("Connected to `" + dbName + "`!");
-    });
+let db,
+  dbConnectionSTR = process.env.DB_STRING
+dbName = "demo"
+
+
+// De1S13ShnAB6yOio: passowrd for server
+
+// const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true"; this was the original url or code that leon had on his code.
+const url = "mongodb+srv://fernandojhernandez123:De1S13ShnAB6yOio@cluster0.78sswj1.mongodb.net/?retryWrites=true&w=majority"
+
+
+app.listen(5050, () => {
+  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+      throw error;
+    }
+    db = client.db(dbName);
+    console.log("Connected to `" + dbName + "`!");
+  });
 });
 
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
+  db.collection('messages').find().sort({ rating: -1 }).toArray((err, result) => {//goes to db (our datbase), goes into the collection 'messages', finds all the documents in the messages collections, plugs those documents/objects into an array with toArray. thumbg 
     if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
+    res.render('index.ejs', { messages: result })//renders the result from the messages collection through ejs so that ejs can spit it out as html. "render our ejs using this data from messages"
   })
 })
 
+
 app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+
+  db.collection('messages').insertOne({ name: req.body.name, rating: req.body.rating, bookReport: req.body.bookReport, likes: 0 }, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
@@ -40,21 +51,42 @@ app.post('/messages', (req, res) => {
 
 app.put('/messages', (req, res) => {
   db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
+    .findOneAndUpdate({ name: req.body.name, rating: req.body.rating, bookReport: req.body.bookReport }, {
+      $inc: {
+        likes: 1
+      }
+    }, {
+      sort: { _id: -1 },
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    })
 })
 
+
+app.put('/messagessort', (req, res) => {
+  db.collection('messages')
+    .sort({ rating: -1 }, {
+      $set: { //$sets something from the database and the code below allows us to SET the thumbs up to its value plus one.  
+        rating: req.body.rating,
+
+      }
+    }, {
+      sort: { _id: -1 },
+      upsert: true
+    }, (err, result) => {
+      if ('name' == '' || rating == '') return res.send(err)
+      res.send(result)
+    })
+})
+
+//app.delete seems to go through on the dom becaiuse it looks like the the event listener is triggered but doesnt actually delete the post. 
+
 app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+  const { name, rating, bookReport, likes } = req.body;
+  console.log(req.body)
+  db.collection('messages').findOneAndDelete({ name, rating, bookReport, likes }, (err, result) => {
     if (err) return res.send(500, err)
     res.send('Message deleted!')
   })
